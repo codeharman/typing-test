c4852c0ad09218096ba7e52a28bc4680c22580a3
Harmanjot Singh <161106030+codeharman@users.noreply.github.com>
2026-03-12 16:33:08 +0530

    Header component contents modified and styled with h1 and p

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div className="nav">
+      <h1 className="nav-header">Typing Test</h1>
+      <p>by <a className="nav-link">@codeharman</a></p>
+    </div>
+  )
+}
